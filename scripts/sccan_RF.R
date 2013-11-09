8cf7a0bf81cd0632d62525af14b530e7eb3e849a
brian avants <stnava@gmail.com>
2013-11-09 10:05:25 -0500

    BUG: was plotting against wrong hrf

--- scripts/sccan_RF.R
+++ scripts/sccan_RF.R
@@ -2,10 +2,13 @@ library( ANTsR )
 library( randomForest)
 library( vegan )
 library( mboost )
-dowhiten <- TRUE
+dowhiten <- FALSE
+dotwhiten <- TRUE
 useglmb <- FALSE
 takeoutresid <- TRUE
-subnum<-"010"
+# subnum<-"005"
+for ( subnum in c("001","002","003","004","005","006","007","008","009","010" ) )
+{
 print(paste("a random forest test on fmri ... takeoutresid? ",takeoutresid,'subject',subnum))
 rootdir<-paste("/Users/stnava/data/data_gorgolewski/RfMRI/")
 gimgfn<-paste(rootdir,"OptGroupTask003Run001sccan.nii.gz",sep='')
@@ -41,11 +44,14 @@ selector[1:(nbadframes-1)] <- FALSE
 mat<-subset( mat , selector )
 nuis<-subset( nuis , selector )
 myhrf<-subset( ohrf , selector )
-nuis<-as.matrix( residuals( lm( as.matrix(nuis) ~ myhrf ) ) )
+myhrfb<-matrix( c( blockStimulus( dim(fmri)[4], blockfing, 6 ), blockStimulus( dim(fmri)[4], blockfoot, 6 ), blockStimulus( dim(fmri)[4], blockmout, 6 ) ) , nrow= dim(fmri)[4] )[selector,]
+# nuis<-as.matrix( residuals( lm( as.matrix(nuis) ~ myhrf ) ) )
+nuis<-as.matrix( residuals( lm( as.matrix(nuis) ~ as.factor(myhrf[,1]) + as.factor(myhrf[,2]) + as.factor(myhrf[,3])) ) )
 if ( takeoutresid )  mat<-( as.matrix( residuals( lm( mat  ~ as.matrix( nuis ) ) ) ) )
 myglobsig<-apply( mat, FUN=mean, MARGIN=1 )
 mat[,1]<-myglobsig
-if ( dowhiten ) mat <- temporalwhiten( mat ) 
+if ( dotwhiten ) mat <- temporalwhiten( mat ) 
+if ( dowhiten ) mat <- whiten( mat ) 
 plot(   myhrf[,1], type='l' )
 points( myhrf[,2], type='l', col='blue' )
 points( myhrf[,3], type='l', col='red' )
@@ -62,7 +68,7 @@ for ( i in 5:nrow(myhrf) )
 points( mysums, type='l', col='green' )
 binpreds <- as.factor( binpreds )
 myglobsigresid<-residuals( lm( myglobsig ~ myhrf ) )
-threshinit<-0.3
+threshinit<-0.2
 img1<-antsImageClone( mask )
 img1[ mask > 0 ]<-sparsify( myhrf[,1] %*% mat , threshinit, mask )
 img2<-antsImageClone( mask )
@@ -71,12 +77,20 @@ img3<-antsImageClone( mask )
 img3[ mask > 0 ]<-sparsify( myhrf[,3] %*% mat , threshinit, mask )
 img4<-antsImageClone( mask )
 img4[ mask > 0 ]<- sparsify( myglobsigresid %*% mat, threshinit, mask )
-img5<-antsImageClone( mask )
-img5[ mask > 0 ]<- sparsifyv( svd( mat )$u[2,] %*% mat, threshinit, mask )
+mysvd<-svd( mat )
 initlist<-list( img1,img2,img3,img4 )
-myhrfb<-matrix( as.numeric( myhrf > 0.5 ) , nrow=nrow(myhrf) )
-mypreds <- cbind(myhrf,myhrfb)
-ff<-sparseDecom2( inmatrix=list(mat,mypreds), inmask=list(mask,NA), perms=0, its=45, mycoption=1, sparseness=c( -0.02, 0.1 ) , nvecs= ncol( mypreds ), smooth=1, cthresh=c(10,0), ell1 = 11 , z=-1 ) #  , initializationList=initlist )
+for ( i in 1:10 ) {
+  img5<-antsImageClone( mask )
+  img5[ mask > 0 ]<- sparsifyv( t(mysvd$v[,i]) , threshinit, mask )
+  initlist<-lappend( initlist, img5 )
+  print( max( img5 ) )
+}
+mypreds <- cbind(myhrf , myhrfb)
+# mypreds <- cbind( myhrfb)
+nv <- length(initlist)
+nv <- ncol( mypreds )
+initlist<-list( img1,img2,img3 )
+ff<-sparseDecom2( inmatrix=list(mat,mypreds), inmask=list(mask,NA), perms=0, its=43, mycoption=1, sparseness=c( -0.02, 0.1 ) , nvecs= nv, smooth=0, robust=0,cthresh=c(10,0), ell1 = 11 , z=-1 ) # , initializationList=initlist )
 print( ff$eig2 )
 if ( FALSE ) {
  ff<-sparseDecom( inmatrix=mat, inmask=mask,its=3,
@@ -94,13 +108,13 @@ for ( i in 1:length(ff$eig1) ) {
 gimgproj <- ( mat ) %*% groupvec
 mysccanpreds <- ( mat  ) %*% t( mysccanimages )
 colnames( mysccanpreds )<-colnames( ff$projections )
-mydf<-data.frame( binpreds1 = myhrf[,1] , imgs = mysccanpreds  , gimgproj = gimgproj )
+mydf<-data.frame( binpreds1 = myhrf[,1] , imgs = mysccanpreds   , gimgproj = gimgproj )
 my.rf1<-randomForest( binpreds1 ~ . , data=mydf )
 if ( useglmb ) my.rf1<-          glmboost( binpreds1 ~ . , data=mydf )
-mydf<-data.frame( binpreds2 = myhrf[,2] , imgs = mysccanpreds  , gimgproj = gimgproj )
+mydf<-data.frame( binpreds2 = myhrf[,2] , imgs = mysccanpreds   , gimgproj = gimgproj )
 my.rf2<-randomForest( binpreds2 ~ . , data=mydf )
 if ( useglmb ) my.rf2<-          glmboost( binpreds2 ~ . , data=mydf )
-mydf<-data.frame( binpreds3 = myhrf[,3] , imgs = mysccanpreds  , gimgproj = gimgproj )
+mydf<-data.frame( binpreds3 = myhrf[,3] , imgs = mysccanpreds   , gimgproj = gimgproj )
 my.rf3<-randomForest( binpreds3 ~ . , data=mydf )
 if ( useglmb ) my.rf3<-          glmboost( binpreds3 ~ . , data=mydf )
 ######################################################################
@@ -118,7 +132,8 @@ nuis2<-as.matrix( residuals( lm( as.matrix(nuis2) ~ myhrf2 ) ) )
 if ( takeoutresid ) mat2<-( as.matrix( residuals( lm( mat2  ~ as.matrix( nuis2 ) ) ) ) )
 myglobsig2<-apply( mat2, FUN=mean, MARGIN=1 )
 mat2[,1]<-myglobsig2
-if ( dowhiten ) mat2 <- temporalwhiten( mat2 ) 
+if ( dotwhiten ) mat2 <- temporalwhiten( mat2 ) 
+if ( dowhiten ) mat2 <- whiten( mat2 ) 
 gimgproj <- ( mat2 )  %*% groupvec
 mysccanpreds <- ( mat2  ) %*% t( mysccanimages )
 colnames( mysccanpreds )<-colnames( ff$projections )
@@ -143,7 +158,9 @@ pdf(paste(rootdir,'/figure/decoding',subnum,".pdf",sep=''),width=7,height=3)
 plot(   mypred1 , type='l' , col='black',lty=2,ylim=c(-1,2),main="BOLD (dashed) decodes stimulus HRF (solid)")
 points(   mypred2 , type='l' , col='red',lty=2)
 points(   mypred3 , type='l' , col='green',lty=2)
-points(   myhrf[,1] , type='l' , col='black',pch=2)
-points(   myhrf[,2] , type='l' , col='red',pch=2)
-points(   myhrf[,3] , type='l' , col='green',pch=2)
+points(   myhrf2[,1] , type='l' , col='black',pch=2)
+points(   myhrf2[,2] , type='l' , col='red',pch=2)
+points(   myhrf2[,3] , type='l' , col='green',pch=2)
 dev.off()
+print( subnum )
+}
